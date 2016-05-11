6340d451596f77687938b45670ee3594a51d6344
Kent Merrell <kmerrell@gmail.com>
2016-05-10 20:40:14 -0400

    working on hover on ListItems

--- app/components/left-side/list-item.js
+++ app/components/left-side/list-item.js
@@ -4,15 +4,41 @@ import React, { Component, PropTypes } from 'react';
 class ListItem extends Component {
   constructor() {
     super(...arguments)
-
+	this.state = { hovered:false }
+	this.onMouseOver = this.onMouseOver.bind(this)
+	this.onMouseOut = this.onMouseOut.bind(this)
+	this.style = this.style.bind(this)
   }
-onMouseOverHandler:function(){
-	
+
+
+
+style() {
+	console.log('style',this.state)
+  if (this.state.hovered) {
+    return { backgroundColor: "red" }
+  } else {
+    return { backgroundColor: "grey" }
+  }
 }
+onMouseOver () {
+	//console.log(this.state)
+  this.state.hovered=true;
+  this.style();
+}
+
+onMouseOut() {
+	//console.log('out')
+	this.state.hovered=false;
+}
+
+
   render() {
 
+
   	return(
-		<li className="has_sub waves-effect waves-primary" onMouseOver={this.onMouseOverHandler}>
+		<li className="has_sub waves-effect waves-primary" onMouseOver={this.onMouseOver} 
+                   onMouseOut={this.onMouseOut} 
+                   style={this.style()}>
             <i className={this.props.selected?"md-radio-button-on":"md-radio-button-off"}></i>{this.props.text}
         </li>
         )
